5cfd4a7ed5263865734e97c0bc538f124e2e3393
Marco Rebhan <me@dblsaiko.net>
2021-07-25 14:20:06 +0200

    Add a test

--- jvmapi/src/lib.rs
+++ jvmapi/src/lib.rs
@@ -1,6 +1,6 @@
+pub mod framed;
 mod javacli;
 mod protocol;
-pub mod framed;
 
 pub mod jvm;
 
@@ -9,3 +9,24 @@ const PATH_SEPARATOR: &str = ":";
 
 #[cfg(any(target_os = "windows", target_os = "redox"))]
 const PATH_SEPARATOR: &str = ";";
+
+#[cfg(test)]
+mod test {
+    use crate::jvm::command::JvmCommand;
+    use crate::jvm::direct::DirectJvm;
+    use crate::jvm::process::ProcessJvm;
+    use crate::jvm::JvmTask;
+
+    #[test]
+    fn spawn_jvm() {
+        let jvm = ProcessJvm::new();
+        let jvm = DirectJvm::spawn(jvm).expect("Failed to spawn JVM");
+
+        let mut task = JvmCommand::new(&jvm, "net.dblsaiko.origami.TestService")
+            .arg("arg1")
+            .spawn()
+            .expect("Failed to spawn task");
+
+        task.wait().expect("Failed to wait for task to exit");
+    }
+}
